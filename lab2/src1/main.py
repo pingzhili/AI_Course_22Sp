5e97002d1c57b33228c6d9839079519170a9d020
pingzhi <pingzhili@mail.ustc.edu.cn>
2022-05-25 12:10:41 +0800

    update lab2

--- lab2/src1/main.py
+++ lab2/src1/main.py
@@ -8,7 +8,7 @@ def test_decisiontree():
     model = DecisionTree()
     model.fit(train_features, train_labels)
     results = model.predict(test_features)
-    model.print_tree()
+    # model.print_tree()
     print('DecisionTree acc: {:.2f}%'.format(get_acc(results, test_labels) * 100))
 
 
@@ -21,7 +21,7 @@ def test_svm(C=1, kernel='Linear', epsilon=1e-4):
 
 
 if __name__ == '__main__':
-    # test_decisiontree() 
+    test_decisiontree() 
     test_svm(1, 'Linear', 1e-4)
     test_svm(1, 'Poly', 1e-4)
     test_svm(1, 'Gauss', 1e-4)
